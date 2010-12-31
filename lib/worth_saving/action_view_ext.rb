6fafdd83853162539b71e71a868c6f7ce0f2514a
John Lawrence <john@yourha.com>
2010-12-31 04:42:29 -0800

    got various form field helpers to produce correct output; still need to write specs for nested attributes and namespaced resources

--- lib/worth_saving/action_view_ext.rb
+++ lib/worth_saving/action_view_ext.rb
@@ -6,25 +6,68 @@ module WorthSaving
   
     module ClassMethods
       def worth_saving_extra_action_view_methods
+        
+        # def setup_options!, given the ! not because it changes the caller but b/c it changes the arguments (options)
+        # The two separate options arrays are the same except in the case of the 'select' field builder, which has
+        #     html_options for HTML attrs rather than using the options hash like the other field builders
+        def setup_options!(object_name, in_options, out_options = nil)
+          out_options ||= in_options
+          object_id = in_options[:object] && in_options[:object].to_param && "_#{in_options[:object].to_param}"
+          if object_name.to_s.camelize.constantize.worth_saving? && in_options.delete(:draft) != false
+            out_options.merge!({ 'data-draft' => object_name + object_id.to_s })
+          end
+        end
+
         include WorthSaving::ActionViewExt::InstanceMethods
 
         alias_method_chain :text_field, :worth_saving_check
         alias_method_chain :select, :worth_saving_check
-        # alias_method_chain :text_area, :worth_saving_check
-        # alias_method_chain :radio_field, :worth_saving_check
+        alias_method_chain :check_box, :worth_saving_check
+        alias_method_chain :text_area, :worth_saving_check
+        alias_method_chain :radio_button, :worth_saving_check
+        
+      #### Unspec'd as of now
+        # alias_method_chain :form_for, :worth_saving_check
       end
     end
     
+    # All of these <something>_with_worth... methods will have a .to_s after object_name.  That should be put into the spec
+    #    so nobody ever fucks it up.  It passes specs without it unless using the method without being inside a form_for block
     module InstanceMethods
+      def draft_message(*args)
+        debugger
+        "This shit has a draft yo"
+      end
+      
       def text_field_with_worth_saving_check(object_name, method, options = {})
-        options.merge!({ 'data-draft' => true }) if object_name.camelize.constantize.worth_saving? && options.delete(:draft) != false
+        ActionView::Base.setup_options!(object_name, options)
         text_field_without_worth_saving_check(object_name, method, options)
       end
 
       def select_with_worth_saving_check(object_name, method, choices, options = {}, html_options = {})
-        html_options.merge!({ 'data-draft' => true }) if object_name.camelize.constantize.worth_saving? && options.delete(:draft) != false
+        ActionView::Base.setup_options!(object_name, options, html_options)
         select_without_worth_saving_check(object_name, method, choices, options, html_options)
       end
+
+      def check_box_with_worth_saving_check(object_name, method, options = {}, checked_value = "1", unchecked_value = "0")
+        ActionView::Base.setup_options!(object_name, options)
+        check_box_without_worth_saving_check(object_name, method, options, checked_value, unchecked_value)
+      end
+
+      def text_area_with_worth_saving_check(object_name, method, options = {})
+        ActionView::Base.setup_options!(object_name, options)
+        text_area_without_worth_saving_check(object_name, method, options)
+      end
+
+      def radio_button_with_worth_saving_check(object_name, method, tag_value, options = {})
+        ActionView::Base.setup_options!(object_name, options)
+        radio_button_without_worth_saving_check(object_name, method, tag_value, options)
+      end
+      
+      # def form_for_with_worth_saving_check(record_or_name_or_array, *args, &proc)
+      #   options.merge!({ 'data-draft' => object_name }) if object_name.camelize.constantize.worth_saving?
+      #   form_for_without_worth_saving_check(record_or_name_or_array, *args, &proc)
+      # end
 
     end
   
--- spec/models/action_view_ext_spec.rb
+++ spec/models/action_view_ext_spec.rb
@@ -19,43 +19,147 @@ describe ActionView::Base do
       Column.new("title",'Default Title',"string",false), 
       Column.new("body",'Default Body',"text",false)
     ])
+    
+    @view = ActionView::Base.new
+    @view.stubs(:worth_saving_records_path).returns('/worth_saving_records')
+    @view.stubs(:records_path).returns('/records')
+    @view.stubs(:protect_against_forgery?).returns(false)
   end
   
   # Some missing specs: test for what happens if we feed in an options hash other than {}
-  #   Currently a worth_saving record will always have data-draft="true" b/c of the options.merge!('data-draft' => true)
+  #   Currently a worth_saving record will always have data-draft="worth_saving_record" b/c of the options.merge!('data-draft' => worth_saving_record)
   #   Quite possbily, however, we would like for a developer to be able to override this in a given form for a given field
-  #     by feeding in 'data-draft' => false
-  describe "when creating a form for a worth_saving record" do
-    
-    it "text_field should include a data-draft attribute with value true" do
-      ActionView::Base.new.text_field('worth_saving_record', :title).should =~ /data-draft="true"/
+  #     by feeding in :draft => false
+  describe "when using form_for on a model that is worth_saving" do
+    before :each do
+      @worth_saving_record = WorthSavingRecord.new
     end
     
+    describe "and the record is new" do
+      @worth_saving_record.stubs(:persisted?).returns(false)
+      it "text_field should include a data-draft attribute with value equal to the name of the model" do
+        @view.form_for @worth_saving_record do |f|
+          f.text_field(:title).should =~ /data-draft="worth_saving_record"/
+        end
+      end
+
+      it "select should include a data-draft attribute with value equal to the name of the model" do
+        @select_options_array = [1,2,3]
+        @view.form_for @worth_saving_record do |f|
+          f.select(:title, @select_options_array).should =~ /data-draft="worth_saving_record"/
+        end
+      end
+
+      it "check_box should include a data-draft attribute with value equal to the name of the model" do
+        @view.form_for @worth_saving_record do |f|
+          f.check_box(:title).should =~ /data-draft="worth_saving_record"/
+        end
+      end
+
+      it "text_area should include a data-draft attribute with value equal to the name of the model" do
+        @view.form_for @worth_saving_record do |f|
+          f.text_area(:title).should =~ /data-draft="worth_saving_record"/
+        end
+      end
+
+      it "radio_button should include a data-draft attribute with value equal to the name of the model" do
+        @view.form_for @worth_saving_record do |f|
+          f.radio_button(:title, 1).should =~ /data-draft="worth_saving_record"/
+        end
+      end
+    end
+    
+    describe "and the record is from the database" do
+      before :each do
+        @worth_saving_record.id = 1254
+      end
+      
+      it "text_field should include a data-draft attribute with value equal to the name of the model with its to_param" do
+        @view.form_for @worth_saving_record do |f|
+          f.text_field(:title).should =~ /data-draft="worth_saving_record_1254"/
+        end
+      end
+      
+      it "select should include a data-draft attribute with value equal to the name of the model with its to_param" do
+        @select_options_array = [1,2,3]
+        @view.form_for @worth_saving_record do |f|
+          f.select(:title, @select_options_array).should =~ /data-draft="worth_saving_record_1254"/
+        end
+      end
+
+      it "check_box should include a data-draft attribute with value equal to the name of the model with its to_param" do
+        @view.form_for @worth_saving_record do |f|
+          f.check_box(:title).should =~ /data-draft="worth_saving_record_1254"/
+        end
+      end
+
+      it "text_area should include a data-draft attribute with value equal to the name of the model with its to_param" do
+        @view.form_for @worth_saving_record do |f|
+          f.text_area(:title).should =~ /data-draft="worth_saving_record_1254"/
+        end
+      end
+
+      it "radio_button should include a data-draft attribute with value equal to the name of the modelwith its to_param" do
+        @view.form_for @worth_saving_record do |f|
+          f.radio_button(:title, 1).should =~ /data-draft="worth_saving_record_1254"/
+        end
+      end
+    end  # describe "and the record is from the database"
+
     it "text_field should not include a data-draft attribute if it is fed :draft => false in the options hash" do
-      ActionView::Base.new.text_field('worth_saving_record', :title, { :draft => false }).should_not =~ /data-draft/      
+      @view.form_for @worth_saving_record do |f|
+        f.text_field(:title, :draft => false ).should_not =~ /data-draft/      
+      end
     end
 
-    it "select should include a data-draft attribute with value true" do
+    it "select should not include a data-draft attribute if it is fed :draft => false in the options hash" do
       @select_options_array = [1,2,3]
-      ActionView::Base.new.select('worth_saving_record', :title, @select_options_array).should =~ /data-draft="true"/
+      @view.form_for @worth_saving_record do |f|
+        f.select(:title, @select_options_array, :draft => false).should_not =~ /data-draft/
+      end
     end
-    
-    it "select should include a data-draft attribute with value true" do
-      @select_options_array = [1,2,3]
-      ActionView::Base.new.select('worth_saving_record', :title, @select_options_array, :draft => false).should_not =~ /data-draft="true"/
+        
+    it "check_box should not include a data-draft attribute if it is fed :draft => false in the options hash" do
+      @view.form_for @worth_saving_record do |f|
+        f.check_box(:title, :draft => false).should_not =~ /data-draft/
+      end
     end
-    
+        
+    it "text_area should not include a data-draft attribute if it is fed :draft => false in the options hash" do
+      @view.form_for @worth_saving_record do |f|
+        f.text_area(:title, :draft => false).should_not =~ /data-draft/
+      end
+    end
+        
+    it "radio_buton should not include a data-draft attribute if it is fed :draft => false in the options hash" do
+      @view.form_for @worth_saving_record do |f|
+        f.radio_button(:title, 1, :draft => false).should_not =~ /data-draft/
+      end
+    end
+        
   end
 
   describe "when creating a form for a record that is NOT worth_saving" do
     
     it "text_field should not include a data-draft attribute" do
-      ActionView::Base.new.text_field('record', :title, {}).should_not =~ /data-draft="true"/
+      ActionView::Base.new.text_field('record', :title, {}).should_not =~ /data-draft/
     end
     
     it "select should not include a data-draft attribute" do
       @select_options_array = [1,2,3]
       ActionView::Base.new.select('record', :title, @select_options_array).should_not =~ /data-draft/
+    end
+    
+    it "check_box should not include a data-draft attribute" do
+      ActionView::Base.new.check_box('record', :title, {}).should_not =~ /data-draft/
+    end
+    
+    it "text_area should not include a data-draft attribute" do
+      ActionView::Base.new.text_area('record', :title, {}).should_not =~ /data-draft/
+    end
+    
+    it "radio_button should not include a data-draft attribute" do
+      ActionView::Base.new.radio_button('record', :title, 1, {}).should_not =~ /data-draft/
     end
     
   end
