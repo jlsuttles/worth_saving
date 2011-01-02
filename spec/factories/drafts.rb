12fe2b2f8b5784f03b62dbb85640c1a054d22f52
John Lawrence <john@yourha.com>
2011-01-02 14:52:44 -0800

    removed cruft from drafts factory

--- spec/factories/drafts.rb
+++ spec/factories/drafts.rb
@@ -1,18 +1,4 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
-Factory.define :page do |f|
-  f.title 'Page Title'
-  f.permalink 'page-title'
-  f.meta_title 'Page Meta Title'
-  f.body 'This page is about some stuff...'
-  f.visible true
-end
-
-Factory.define :page_draft, :class => Draft do |f|
-  f.record_type "Page"
-  f.sequence(:record_id) { |n| "page-title#{n}" }
-  f.sequence(:content) { |n| Factory.create(:page, {:permalink => "page-title#{n}"}).to_json }
-end
-
 Factory.define :draft do |f|
   f.record_type nil
   f.record_id nil
